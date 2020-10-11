c942c56bbf3e312d15409c0d0b89ba336ed41a86
Marlon81785 <marlon81785@gmail.com>
2020-10-11 20:35:49 -0300

    opa

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ now = new Date
 
 
 //como usar no frontend
-//document.write ("<h1> Hoje é " + dayName[now.getDay() ] + ", " + now.getDate () + " de " + monName [now.getMonth() ]   +  " de "  +     now.getFullYear () + ". </h1>")
+document.write ("<h1> Hoje  " + dayName[now.getDay() ] + ", " + now.getDate () + " de " + monName [now.getMonth() ]   +  " de "  +     now.getFullYear () + ". </h1>")
 
 
 //data na receita
@@ -12,7 +12,7 @@ var dia = null;
 var mes = null;
 var ano = now.getFullYear();
 
-if(now.getDate() < 10){ dia = "0"+now.getDate(); }
-if(now.getMonth() < 10){ mes = "0"+now.getMonth(); mes++; }
+if(now.getDate() < 10){ dia = "0"+now.getDate(); }else{ dia = now.getDate(); }
+if(now.getMonth() < 10){ mes = "0"+now.getMonth(); mes++; }else{ dia = now.getMonth(); }
 
 var dataReceita = dia+"/"+mes+"/"+ano;
